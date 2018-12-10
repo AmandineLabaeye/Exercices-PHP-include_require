58c48d03a860eedc7e4968819a0c5df868c452d2
sstienface <s.stien@fondationface.org>
2018-12-10 11:49:54 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sstienface
+ * Date: 04/12/2018
+ * Time: 11:25
+ */
+
+// Premiere ligne
+
+echo"Un fichier php va être inclu à la suite de ce texte ...<br><br>";
+
+    // inclure fichier texte.php ci dessous
+
+
+// Deuxieme Ligne ( inclure fichiers vars.php ci dessous )
+
+
+
+echo"J'aime également les ".$maVar;
+
+
+
+
+
+
